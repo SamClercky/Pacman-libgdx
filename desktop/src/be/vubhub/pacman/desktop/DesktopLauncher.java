9f841ee55c18f0c55b860fbb4208c9a46dea2813
SamClercky <declerck.wim@gmail.com>
2020-02-15 11:51:22 +0100

    initial commit 2.0

---
+++ desktop/src/be/vubhub/pacman/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package be.vubhub.pacman.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import be.vubhub.pacman.Pacman;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Pacman(), config);
+	}
+}
